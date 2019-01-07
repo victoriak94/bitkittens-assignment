3034f307d15853b15ccd7949483bcdb5115faf83
victoriak94 <vkamila6@gmail.com>
2019-01-07 16:08:52 -0500

    Save summon kitties button in variable. Add click event to button that makes get request to kittens data. Return response data in json. Save all divs in one catBoxes variable. Iterate over each div and append a cat image into each box. Complete assignment.

---
+++ cats.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  const summonKittiesButton = document.querySelector('button.summon-cats');
+  const catBoxes = document.querySelectorAll("div.cat-box");
+
+  summonKittiesButton.addEventListener('click', () => {
+    console.log('Summon all the cats!')
+    axios({
+      method: 'GET',
+      url: 'http://bitkittens.herokuapp.com/cats' + '.json',
+      responseType: 'json'
+    }).then((response) => {
+      console.log(response.data)
+
+      catBoxes.forEach((box, index) => {
+        box.innerHTML = '';
+        const catImage = document.createElement('img');
+        catImage.src = response.data.cats[index].photo;
+        catImage.alt = `Photo of ${response.data.cats[index].name}`;
+        box.appendChild(catImage);
+      });
+    });
+  });
+
+});
